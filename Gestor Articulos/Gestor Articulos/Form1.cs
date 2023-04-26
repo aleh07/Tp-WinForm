82c2020b05710b36b804afd990bae2de9272b3d9
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2023-04-25 21:10:09 -0300

    Creacion del proyecto ya en capas y con algunas clases

---
+++ Gestor Articulos/Gestor Articulos/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestor_Articulos
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void listarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
