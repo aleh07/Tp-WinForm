335318962740cee59a6782bad250dd860376be61
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2023-04-26 20:47:49 -0300

    Creacion de clases producto , marca , categoria , imagen y acceso datos

--- Gestor Articulos/Dominio/Producto.cs
+++ Gestor Articulos/Dominio/Producto.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    class Producto
+    public class Producto
     {
-    }
+        public Int32 Id { get; set; }
+        public Int64 CodArtículo{ get; set; }
+        public string Nombre { get; set; }
+        public string Descripción { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria{ get; set; }
+
+        public string Imagen { get; set; }
+        public Decimal Precio { get; set; }
+
+     
 }
