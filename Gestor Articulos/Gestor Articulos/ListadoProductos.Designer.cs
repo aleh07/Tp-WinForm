85f4cb890dbb85c7f7f4dd4f06533fd2d81f4285
AgustinaRios <agus.riosv@gmail.com>
2023-04-29 19:28:55 -0300

    agregado data grid view

--- Gestor Articulos/Gestor Articulos/ListadoProductos.Designer.cs
+++ Gestor Articulos/Gestor Articulos/ListadoProductos.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvProducto = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducto)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProducto
+            // 
+            this.dgvProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducto.Location = new System.Drawing.Point(0, 0);
+            this.dgvProducto.Name = "dgvProducto";
+            this.dgvProducto.Size = new System.Drawing.Size(240, 150);
+            this.dgvProducto.TabIndex = 0;
+            this.dgvProducto.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // ListadoProductos
             // 
@@ -37,13 +48,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProducto);
             this.Name = "ListadoProductos";
             this.Text = "ListadoProductos";
             this.Load += new System.EventHandler(this.ListadoProductos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducto)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducto;
     }
 }
--- Gestor Articulos/Gestor Articulos/ListadoProductos.cs
+++ Gestor Articulos/Gestor Articulos/ListadoProductos.cs
@@ -22,12 +22,30 @@ namespace Gestor_Articulos
 
         private void ListadoProductos_Load(object sender, EventArgs e)
         {
-            //ProductoNegocio negocio = new ProductoNegocio();
-            //listaProductos = negocio.listar();
-            
+            ProductoNegocio negocio = new ProductoNegocio();
+            try
+            {
+                listaProductos = negocio.listar();
+                dgvProducto.DataSource = listaProductos;
+                dgvProducto.Columns["id"].Visible= false;
+                dgvProducto.Columns["nombre"].Visible = false;
+                dgvProducto.Columns["descripcion"].Visible = false;
+                dgvProducto.Columns["marca"].Visible = false;
+                dgvProducto.Columns["categoria"].Visible = false;
+                dgvProducto.Columns["ImgArt"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
